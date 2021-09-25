aaeb1a06d4086d1431791b10fb6b8cbaf1825d99
Phillip <phillip.welch2@mavs.uta.edu>
2021-09-25 01:00:51 -0500

    Working generator now to add detail (fix UI, be able to repeat generate).

--- script.js
+++ script.js
@@ -6,10 +6,11 @@ var passwordRequirements = {
   lowercase : false,
   uppercase : false,
   special : false,
-  passwordProto : "",
+  passwordProto : [],
 };
 
 function passwordStats() {
+  console.log("event listener 2 activate");
   /*while(((passwordRequirements.length < 8) || (passwordRequirements.length > 128)) && ((passwordRequirements.lowercase == false) && (passwordRequirements.uppercase == false) && (passwordRequirements.special == false))) {
     passwordRequirements.length = parseInt(document.getElementById("inputlength").value);
     passwordRequirements.lowercase = document.getElementById("inputlc").checked;
@@ -23,34 +24,39 @@ function passwordStats() {
       window.alert("Must check atleast one!!!");
     }
   }*/
+
   passwordRequirements.length = parseInt(document.getElementById("inputlength").value);
-  lowercase = document.getElementById("inputlc").checked;
-  uppercase = document.getElementById("inputup").checked;
-  special = document.getElementById("inputsp").checked;
+  passwordRequirements.lowercase = document.getElementById("inputlc").checked;
+  passwordRequirements.uppercase = document.getElementById("inputup").checked;
+  passwordRequirements.special = document.getElementById("inputsp").checked;
 
   var showForm = document.querySelector("#passwordform");
   showForm.setAttribute("style", "display: none;");
+
+  writePassword();
 }
 
 function userInfo() {
+  console.log("event listener activate");
   var showForm = document.querySelector("#passwordform");
   showForm.setAttribute("style", "display: block;");
   
   var submitBtn = document.querySelector("#submit");
   submitBtn.addEventListener("click", passwordStats);
-  
-  writePassword();
 }
 
 function generatePassword() {
-  var lowercaseChoices = "abcdefghijklmnopqrstuvwxyz".split("");
-  var uppercaseChoices = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
-  var specialChoices = "!@#$%^&*()?".split("");
+  console.log("generate password function triggered");
+  var lowercaseChoices = "abcdefghijklmnopqrstuvwxyz";
+  var uppercaseChoices = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var specialChoices = "!@#$%^&*()?";
 
   var Choices = "";
+  console.log(Choices);
 
   if(passwordRequirements.lowercase == true) {
     Choices += lowercaseChoices;
+    console.log("lowercase");
   }
   if(passwordRequirements.uppercase == true) {
     Choices += uppercaseChoices;
@@ -59,15 +65,24 @@ function generatePassword() {
     Choices += specialChoices;
   }
 
+  console.log(Choices);
+  var choiceArray = Choices.split("");
+  console.log(choiceArray);
+
+  Choices = "";
+
   for(let i = 0; i < passwordRequirements.length; i++) {
-    passwordRequirements.passwordProto += Math.floor(Math.random() * Choices.length);
+    passwordRequirements.passwordProto.push(choiceArray[Math.floor(Math.random() * choiceArray.length)]);
+    Choices += passwordRequirements.passwordProto[i];
   }
 
-  return passwordRequirements.passwordProto;
+  console.log(Choices);
+  return Choices;
 };
 
 // Write password to the #password input
 function writePassword() {
+  console.log("write password function triggered");
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
